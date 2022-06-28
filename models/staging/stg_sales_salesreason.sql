d50e9da95c21d5af2473aa0eb0afc1e7832a8e41
rodrigo.obregon@hotmail.com <rodrigo.obregon@hotmail.com>
2022-06-28 19:01:11 +0000

    dim product update

--- models/staging/stg_sales_salesreason.sql
+++ models/staging/stg_sales_salesreason.sql
@@ -1,6 +1,6 @@
 with source_data as (
     select
-        salesreasonid
+        salesreasonid as salesreason_id
         , name as reason_name
         , reasontype
     from {{source('adventureworks_data','sales_salesreason')}}
