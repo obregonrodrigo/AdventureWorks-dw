826f89e9598dddfff93667ccae7abcbc938f6f71
rodrigo.obregon@hotmail.com <rodrigo.obregon@hotmail.com>
2022-06-28 21:06:08 +0000

    dim update

---
+++ models/marts/dim_customer.sql
@@ -0,0 +1,38 @@
+with select_customer as (
+
+    select
+        customerid
+        , personid
+        , storeid
+        , territoryid				
+    
+    from {{ ref('stg_sales_customer') }}
+    where personid !=0
+
+)
+    , select_person as (
+        select
+            businessentityid
+            , persontype
+            , full_name
+            , emailpromotion			
+    
+        from {{ ref('stg_person_person') }}
+    )
+    , transformed as (
+        select
+            row_number() over (order by customerid) as customer_sk -- auto-incremental surrogate key
+            , select_customer.customerid
+            , select_customer.storeid
+            , select_customer.territoryid
+            , select_person.businessentityid
+            , select_person.full_name
+            , select_person.emailpromotion
+
+        from select_customer
+            left join select_person
+                on select_person.businessentityid = select_customer.personid
+
+)
+
+select  * from transformed
