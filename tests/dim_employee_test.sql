8579548450c8bd0633fe03cdeb6394268a90cdf0
rodrigo.obregon@hotmail.com <rodrigo.obregon@hotmail.com>
2022-07-21 02:54:17 +0000

    settings

--- tests/dim_employee_test.sql
+++ tests/dim_employee_test.sql
@@ -3,7 +3,7 @@ with
         SELECT
             count(businessentityid) as total
         FROM {{ ref ('dim_employee') }}
-        WHERE hiredate = '2008-03-17' AND businessentityid = 62
+        WHERE hiredate = '2012-05-30' AND businessentityid = 289
     )
 
 select * from count_employee where total != 1
--- tests/fact_sales_test_three.sql
+++ tests/fact_sales_test_three.sql
@@ -7,4 +7,4 @@ with
         WHERE orderdate between '2012-12-01' and '2012-12-31' 
     )
 
-select * from sum_order where total != 1257847.1713999854
+select * from sum_order where total != 1257847.1713999857
